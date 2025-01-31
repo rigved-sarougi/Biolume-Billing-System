e976fa311011d09494baadf45e1946e1947a9c3e
rigved-sarougi <dataanalyst@biolume.in>
2025-01-31 18:06:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ West Velachery, Chennai - 600042.
 GSTIN/UIN: 33AAGFK1394P1ZX
 State Name : Tamil Nadu, Code : 33
 """
-company_logo = 'Untitled design (3).png'
+company_logo = 'mkstxbiolume.png'
 photo_logo = '10.png'
 
 bank_details = """
